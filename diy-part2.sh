e8930749deffe5af4e2927126867913811a20d67
fzsource <63889910+fzsource@users.noreply.github.com>
2025-01-26 07:33:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 # 设置默认IP地址
-sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.18.253/g' package/base-files/files/bin/config_generate
 
 # 清除登陆密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
